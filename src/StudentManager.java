c48ecd96c6163cdd77bf7687f51805985b079681
Trần Bình Kha <tbkha1609@gmail.com>
2021-11-29 23:26:11 +0700

    a lot of bugs

--- src/StudentManager.java
+++ src/StudentManager.java
@@ -1,4 +1,6 @@
-/*
+
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class StudentManager {
@@ -8,12 +10,17 @@ public class StudentManager {
 	// Add thong tin student vao list
 	public void add() {
 		// Nhap id name age address gpa
-		int id = scanner.nextInt();
+		System.out.print("Input id ");
+		int id = getId();
 		scanner.nextLine();
+		System.out.print("Input id ");
 		String name = scanner.nextLine();
+		System.out.print("Input id ");
 		byte age = scanner.nextByte();
 		scanner.nextLine();
+		System.out.print("Input id ");
 		String address = scanner.nextLine();
+		System.out.print("Input id ");
 		float gpa = scanner.nextFloat();
 		
 		Student student = new Student(id, name, age, address, gpa);
@@ -22,5 +29,50 @@ public class StudentManager {
 		studentList.add(student);
 	}
 	
+	// Lay id cua student
+	public int getId() {
+		int id = scanner.nextInt();
+		return id;
+	}
+	
+	// Xoa thong tin student theo id
+	public void delete(int id) {
+		Student student = null;
+		
+		for (int i = 0; i < studentList.size(); i++) {
+			if (studentList.get(i).getId() == id) { // Neu tim thay id
+				student = studentList.get(i);
+				break;
+			}
+		}
+		if (student != null) { // Neu tim thay id
+			studentList.remove(student); // Xoa student
+		}
+		else { // Neu khong tim thay
+			System.out.println("Id not existed!");
+		}
+	}
+	
+	// Sort student by name
+	public void sortStudentByName() {
+		Collections.sort(studentList, new SortByName());
+	}
+	
+	// Sort student by GPA
+	public void sortStudentByGPA() {
+		Collections.sort(studentList, new SortByGPA());
+	}
+	
+	// Show thong tin student
+	public void show() {
+		for (Student student: studentList) {
+			System.out.printf("%5d | ", student.getId());
+			System.out.printf("%20s | ", student.getName());
+			System.out.printf("%5d | ", student.getAge());
+			System.out.printf("%20s | ", student.getAddress());
+			System.out.printf("%10.1f%n | ", student.getGPA());
+		}
+	}
+	
 }
-*/
+
--- src/main.java
+++ src/main.java
@@ -1,8 +1,50 @@
+import java.util.Scanner;
 
 public class main {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String choose = null; // Bien choose dung de nhap menu
+		boolean exit = false; // Kiem tra exit
+		StudentManager studentManager = new StudentManager();
 		
+		
+		showMenu();
+		
+		// Nhap du lieu
+		while (true) {
+			choose = scanner.nextLine();
+			switch(choose) {
+			case "1": // Add student
+				studentManager.add();
+				break;
+			case "2": // Edit student by id
+				break;
+			case "3": // Delete student by id
+				studentManager.delete(studentManager.getId());
+				break;
+			case "4": // Sort student by gpa
+				studentManager.sortStudentByGPA();
+				break;
+			case "5": // Sort student by name
+				studentManager.sortStudentByName();
+				break;
+			case "6": // Show student 
+				studentManager.show();
+				break;
+			case "0": // Exit program
+				System.out.println("Exited!");
+				exit = true;
+				break;
+			default:
+				System.out.println("invalid! please choose action in below menu:");
+				break;
+			}
+			if (exit) {
+				break;
+			}
+			
+		}
 		
 
 	}
@@ -13,7 +55,7 @@ public class main {
 		System.out.println("2. Edit student by id.");
 		System.out.println("3. Delete student by id.");
 		System.out.println("4. Sort student by gpa.");
-		System.out.println("5. Sort student by name/");
+		System.out.println("5. Sort student by name.");
 		System.out.println("6. Show student.");
 		System.out.println("0. Exit.");
 		System.out.println("---------------------------");
